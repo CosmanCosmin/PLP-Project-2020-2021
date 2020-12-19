7c616723116999248472eaeaeb0c1f609731bd98
Cosman Ilie-Cosmin <75634587+CosmanCosmin@users.noreply.github.com>
2020-12-19 22:07:29 +0200

    Add files via upload

--- Project.v
+++ Project.v
@@ -7,35 +7,59 @@ Open Scope string_scope.
 Require Import List.
 Import ListNotations.
 
-Definition Var := string.
+Inductive AExp :=
+| const : nat -> AExp
+| var : string -> AExp
+| plus : AExp -> AExp -> AExp
+| minus : AExp -> AExp -> AExp
+| times : AExp -> AExp -> AExp
+| divide : AExp -> AExp -> AExp
+| modulus : AExp -> AExp -> AExp.
 
-Inductive Exp :=
-| const : nat -> Exp
-| id : Var -> Exp
-| plus : Exp -> Exp -> Exp
-| minus : Exp -> Exp -> Exp
-| times : Exp -> Exp -> Exp
-| divide : Exp -> Exp -> Exp
-| modulus : Exp -> Exp -> Exp.
-
-Coercion const : nat >-> Exp.
-Coercion id : Var >-> Exp.
+Coercion const : nat >-> AExp.
+Coercion var : string >-> AExp.
 Notation "A +' B" := (plus A B) (at level 50).
+Notation "A ++'" := (plus A 1) (at level 50).
 Notation "A -' B" := (minus A B) (at level 50).
+Notation "A --'" := (minus A 1) (at level 50).
 Notation "A *' B" := (times A B) (at level 48).
+Notation "A **'" := (times A A) (at level 48).
 Notation "A /' B" := (divide A B) (at level 48).
 Notation "A %' B" := (modulus A B) (at level 48).
 
-Fixpoint interpret (e : Exp) (env : Var -> nat) : nat :=
-  match e with
-  | const c => c
-  | id x => (env x)
-  | plus e1 e2 => (interpret e1 env) + (interpret e2 env)
-  | minus e1 e2 => (interpret e1 env) - (interpret e2 env)
-  | times e1 e2 => (interpret e1 env) * (interpret e2 env)
-  | divide e1 e2 => Nat.div (interpret e1 env) (interpret e2 env)
-  | modulus e1 e2 => Nat.modulo (interpret e1 env) (interpret e2 env)
-  end.
-Definition env0 := fun x => if string_dec x "x" then 10 else 0.
+Inductive BExp :=
+| btrue : BExp
+| bfalse : BExp
+| bless : AExp -> AExp -> BExp
+| bgreat: AExp -> AExp -> BExp
+| bequ : AExp -> AExp -> BExp
+| bnot : BExp -> BExp
+| band : BExp -> BExp -> BExp
+| bor : BExp -> BExp -> BExp.
+Notation "A <' B" := (bless A B) (at level 52).
+Notation "A >' B" := (bgreat A B) (at level 52).
+Notation "A ==' B" :=  (bequ A B) (at level 52).
+Notation "!' A" := (bnot A) (at level 52).
+Notation "A >=' B" := (bnot (bless A B)) (at level 52).
+Notation "A <=' B" := (bnot (bgreat A B)) (at level 52).
+Notation "A &&' B" := (band A B) (at level 52).
+Notation "A ||' B" := (bnot A B) (at level 52).
 
-Compute (interpret (10 /' 2) env0).
+Definition Params := list string.
+Inductive State :=
+| declareNat : string -> State
+| assignNat : string -> AExp -> State
+| declareBool : string -> State
+| assignBool : string -> BExp -> State
+| callFunction : string -> Params -> State
+| sequence : State -> State -> State
+| while : BExp -> State -> State
+| ifthen : BExp -> State -> State
+| ifelse : BExp -> State -> State -> State.
+
+
+
+
+
+
+
